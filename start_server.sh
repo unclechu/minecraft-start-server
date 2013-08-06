ddd1143123e9e0c682beb6942910894c7bbd8b85
Viacheslav Lotsmanov (unclechu) <lotsmanov89@gmail.com>
2013-08-07 01:45:41 +0400

    Version 0.3

--- start_server.sh
+++ start_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # MineCraft server daemon starting and world backuping script
-# Version: 0.2
+# Version: 0.3
 # Author: Viacheslav Lotsmanov (unclechu) <lotsmanov89@gmail.com>
 # WWW: https://github.com/unclechu/minecraft-start-server
 # License: MIT
